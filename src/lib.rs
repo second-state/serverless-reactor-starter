6461eeb5fdde0c0de4d8b9d4b233b9c9a323945c
wangshishuo <wangshishuo@gmail.com>
2021-04-29 10:12:47 +0000

    change username to user_info

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use wasm_bindgen::prelude::*;
 use meval;
 
 #[wasm_bindgen]
-pub fn text_received(msg: String, _username: String, _step_data: String) -> String {
+pub fn text_received(msg: String, _user_info: String, _step_data: String) -> String {
   let x = meval::eval_str(&msg).unwrap();
   return format!("{}", x);
 }
