332a937f68317b35019f3f55553fc89e8f05d9cc
robnanarivo <robby.xi.qiu@gmail.com>
2021-07-28 13:23:50 +0800

    Release version

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,18 @@
 use rand::Rng;
 use serde_json::Value;
+use ssvm_process_interface::Command;
 use std::str;
 use wasm_bindgen::prelude::*;
-// for offline test:
-// use wasmedge_process_interface::Command;
-use ssvm_process_interface::Command;
 
 #[wasm_bindgen]
 pub fn text_received(msg: String, _user_info: String, _step_data: String) -> String {
   let mut rng = rand::thread_rng();
   let page_number = rng.gen_range(0..6198);
   let mut cmd = Command::new("http_proxy");
-  cmd
-    .arg("get")
-    .arg(format!(
-      "https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,image_id&limit=1&page={}",
-      page_number
-    ));
+  cmd.arg("get").arg(format!(
+    "https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,image_id&limit=1&page={}",
+    page_number
+  ));
   let out = cmd.output();
   if out.status != 0 {
     println!("Code: {}", out.status);
@@ -28,17 +24,20 @@ pub fn text_received(msg: String, _user_info: String, _step_data: String) -> Str
   let artwork = str::from_utf8(&out.stdout).unwrap();
   let artwork: Value = serde_json::from_str(artwork).unwrap();
 
-  // println!("{}", artwork);
-
-  // TODO: if image_id is null --> request again
+  // if image-id is null
   if artwork["data"][0]["image_id"] == Value::Null {
     return String::from("No image_id available. Please try again.");
   }
 
+  let title = artwork["data"][0]["title"].as_str().unwrap();
+  let artist = artwork["data"][0]["artist_display"].as_str().unwrap();
   let image_url = format!(
     "https://www.artic.edu/iiif/2/{}/full/843,/0/default.jpg",
     artwork["data"][0]["image_id"].as_str().unwrap()
   );
 
-  return format!("The artwork URL is <{}>", image_url);
+  return format!(
+    "*Art title*: {}\n*Artist*: {}\n*Art image URL*: <{}>",
+    title, artist.replace("\n", ", "), image_url
+  );
 }
